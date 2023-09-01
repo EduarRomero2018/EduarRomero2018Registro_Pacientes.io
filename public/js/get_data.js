b83192698fe9646779595409c674134ecdf4fbbd
Eduar Romero <el.g21@hotmail.com>
2023-08-31 21:34:09 -0500

    Se Agregó el metodo imprimir y se modificó el html

--- public/js/get_data.js
+++ public/js/get_data.js
@@ -1,17 +1,36 @@
-// function getform(params) {
-  const nombres = document.querySelector("#nombres");
-  const apellidos = document.querySelector("#apellidos_paciente");
-  const email_paciente = document.querySelector("#email_paciente");
-  const direccion_paciente = document.querySelector("#direccion_paciente");
-  const tipo_documento = document.querySelector("#tipo_documento");
-  const numero_documento = document.querySelector("#numero_documento");
-  const buttom = document.querySelector("#save_form");
-// }
+const nombres = document.querySelector("#nombres");
+const apellidos = document.querySelector("#apellidos_paciente");
+const email_paciente = document.querySelector("#email_paciente");
+const direccion_paciente = document.querySelector("#direccion_paciente");
+const tipo_documento = document.querySelector("#tipo_documento");
+const numero_documento = document.querySelector("#numero_documento");
+const buttom = document.querySelector("#save_form");
+// obtener id de paciente
+const listarpaciente = document.querySelector('#listar_pacientes')
 
-buttom.addEventListener("click", function (e) {
-  e.preventDefault();
+// obtener id de paciente que viene del HTML
+const visualizarpacientes = document.querySelector('#visualizarpacientes')
+
+
+buttom.addEventListener('click', datospacientes)
+
+
+function validar(params) {
+
+  if (buttom) {
+
+  } else {
+
+  }
+}
+
+// Creamos nuestra función
+function datospacientes(params) {
+
+  params.preventDefault();
+
   // Creamos nuestro objeto
-  let dataobjet = {
+  let nuevoRegistro = {
     nombre: nombres.value,
     apellidos: apellidos.value,
     email_paciente: email_paciente.value,
@@ -20,24 +39,103 @@ buttom.addEventListener("click", function (e) {
     numero_documento: numero_documento.value
   }
 
-  // localStorage.setItem(numero_documento.value, JSON.stringify(dataobjet))
+  // Obtenemos los registros existentes del localStorage
+  let registrosAlmacenados = JSON.parse(localStorage.getItem("registros"))
 
-  // nombres.value ="";
-  // apellidos.value ="";
-  // email_paciente.value ="";
-  // direccion_paciente.value ="";
-  // tipo_documento.value ="";
-  // numero_documento.value ="";
+  // validamos si viene vacio o no
+  if (!registrosAlmacenados) {
+    registrosAlmacenados = [];
 
-  console.log(dataobjet);
-});
+  } else {
+    // Agregar el nuevo registro al arreglo
+    registrosAlmacenados.push(nuevoRegistro);
+    // Guardar los registros actualizados en el localStorage
+    localStorage.setItem("registros", JSON.stringify(registrosAlmacenados));
+    // vaceamos los valores de los input
+    nombres.value = "";
+    apellidos.value = "";
+    email_paciente.value = "";
+    direccion_paciente.value = "";
+    tipo_documento.value = "";
+    numero_documento.value = "";
+
+    // enviamos si fue guardado con exito
+    Swal.fire({
+      title: 'listo',
+      text: 'El Paciente fue guardado con Éxito',
+      icon: 'success',
+      confirmButtonText: '¡Entendido!'
+    });
+  }
 
 
+}
 
-// function ListarPacientes(params) {
-//   document.querySelector("#listar_pacientes").addEventListener('click', ()=>{
-//     const pacientes = JSON.parse(localStorage.getItem('dataobjet'))
-//     alert('Presionaste el Botón de listar paciente')
-//   }
-// }
-// console.log('Listar Pacientes')
+// escuchamos cuando le den clik al boton listar pacientes
+listarpaciente.addEventListener('click', ImprimirListado)
+function ImprimirListado() {
+
+  let registrosAlmacenados = JSON.parse(localStorage.getItem("registros"))
+
+  if (!registrosAlmacenados) {
+    Swal.fire({
+      title: '¡Ups!',
+      text: 'En el momento no tienes información guardada en tu LocalStorage',
+      icon: 'warning',
+      confirmButtonText: '¡Entendido!'
+    });
+    nombres.value = "";
+    apellidos.value = "";
+    email_paciente.value = "";
+    direccion_paciente.value = "";
+    tipo_documento.value = "";
+    numero_documento.value = "";
+
+  } else {
+    console.log(registrosAlmacenados);
+
+    visualizarpacientes.innerHTML = registrosAlmacenados.map((registro, index) => {
+
+      // insertamos el html
+      return `
+        <div class='card'>
+                                    <table class="table table-light">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Nombres</th>
+                                                <th scope="col">Apellidos</th>
+                                                <th scope="col">Correo</th>
+                                                <th scope="col">Direccion Paciente</th>
+                                                <th scope="col">Tipo Documento</th>
+                                                <th scope="col">Numero Documento</th>
+                                                <th scope="col">Editar</th>
+                                                <th scope="col">Eliminar</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="">
+                                                <td scope="row">${registro.nombre}</td>
+                                                <td>${registro.apellidos}</td>
+                                                <td>${registro.email_paciente}</td>
+                                                <td>${registro.direccion_paciente}</td>
+                                                <td>${registro.tipo_documento}</td>
+                                                <td>${registro.numero_documento}</td>
+                                                <td><button type="button" class="btn btn-outline-secondary" onClick="editar(${index})">Editar</button></td>
+                                                <td> <button type="button" class="btn btn-outline-secondary" onClick="editar(${index})">Eliminar</button></td>
+                                            </tr>
+                                        </tbody>
+                                    </table
+        </div>
+        </div>
+        `
+    }).join('<hr />')
+  }
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/previousElementSibling
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/lastElementChild
+// https://www.instagram.com/p/CurOIHCvZrI/?img_index=10
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/firstElementChild
+// https://www.youtube.com/watch?v=zeb5PsxWijY&t=2895s
