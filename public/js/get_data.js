7e3448c03cc807bece271af8f57fb72d1828cc59
Eduar Romero <el.g21@hotmail.com>
2023-09-03 01:48:48 -0500

    Se le hicieron Mejoras al codigo
    * se corrigió el listar pacientes para que no imprimiera por cada registro una tabla
    * se depuró el código JS y se dejó funcional

--- public/js/get_data.js
+++ public/js/get_data.js
@@ -5,32 +5,32 @@ const direccion_paciente = document.querySelector("#direccion_paciente");
 const tipo_documento = document.querySelector("#tipo_documento");
 const numero_documento = document.querySelector("#numero_documento");
 const buttom = document.querySelector("#save_form");
-// obtener id de paciente
-const listarpaciente = document.querySelector('#listar_pacientes')
+
 
 // obtener id de paciente que viene del HTML
-const visualizarpacientes = document.querySelector('#visualizarpacientes')
+const visualizarpacientes = document.querySelector('#date_object')
 
 
-buttom.addEventListener('click', datospacientes)
-
-
-function validar(params) {
-
-  if (buttom) {
-
-  } else {
-
-  }
-}
+buttom.addEventListener('click', datospacientes);
 
 // Creamos nuestra función
-function datospacientes(params) {
+function datospacientes(event) {
+  event.preventDefault();
 
-  params.preventDefault();
+  // funciona
+  // console.log(nuevoRegistro);
+
+  // Obtenemos los registros existentes del localStorage
+  let registrosAlmacenados = JSON.parse(localStorage.getItem("clave"))
+  if (registrosAlmacenados == null) {
+    alert('No hay datos');
+  } else {
+    alert('Si hay datos');
+  }
+  console.log(registrosAlmacenados);
 
   // Creamos nuestro objeto
-  let nuevoRegistro = {
+  const nuevoRegistro = {
     nombre: nombres.value,
     apellidos: apellidos.value,
     email_paciente: email_paciente.value,
@@ -39,43 +39,44 @@ function datospacientes(params) {
     numero_documento: numero_documento.value
   }
 
-  // Obtenemos los registros existentes del localStorage
-  let registrosAlmacenados = JSON.parse(localStorage.getItem("registros"))
-
   // validamos si viene vacio o no
   if (!registrosAlmacenados) {
     registrosAlmacenados = [];
-
+    registrosAlmacenados.push(nuevoRegistro);
   } else {
     // Agregar el nuevo registro al arreglo
     registrosAlmacenados.push(nuevoRegistro);
-    // Guardar los registros actualizados en el localStorage
-    localStorage.setItem("registros", JSON.stringify(registrosAlmacenados));
-    // vaceamos los valores de los input
-    nombres.value = "";
-    apellidos.value = "";
-    email_paciente.value = "";
-    direccion_paciente.value = "";
-    tipo_documento.value = "";
-    numero_documento.value = "";
-
-    // enviamos si fue guardado con exito
-    Swal.fire({
-      title: 'listo',
-      text: 'El Paciente fue guardado con Éxito',
-      icon: 'success',
-      confirmButtonText: '¡Entendido!'
-    });
   }
+  // Guardar la lista actualizada en el localStorage
+  localStorage.setItem("clave", JSON.stringify(registrosAlmacenados));
+  console.log(registrosAlmacenados);
 
 
+  Swal.fire({
+    title: 'listo',
+    text: 'El Paciente fue guardado con Éxito',
+    icon: 'success',
+    confirmButtonText: '¡Entendido!'
+  });
+
+  // vaceamos los valores de los input
+  nombres.value = "";
+  apellidos.value = "";
+  email_paciente.value = "";
+  direccion_paciente.value = "";
+  tipo_documento.value = "Tipo Documento";
+  numero_documento.value = "";
 }
 
+// obtener id de paciente
+const listarpaciente = document.querySelector('#listar_pacientes')
 // escuchamos cuando le den clik al boton listar pacientes
 listarpaciente.addEventListener('click', ImprimirListado)
-function ImprimirListado() {
+console.log('diste click');
 
-  let registrosAlmacenados = JSON.parse(localStorage.getItem("registros"))
+// funcion para imprimir el listado que esta guardado
+function ImprimirListado() {
+  let registrosAlmacenados = JSON.parse(localStorage.getItem("clave"))
 
   if (!registrosAlmacenados) {
     Swal.fire({
@@ -84,6 +85,7 @@ function ImprimirListado() {
       icon: 'warning',
       confirmButtonText: '¡Entendido!'
     });
+
     nombres.value = "";
     apellidos.value = "";
     email_paciente.value = "";
@@ -94,25 +96,10 @@ function ImprimirListado() {
   } else {
     console.log(registrosAlmacenados);
 
-    visualizarpacientes.innerHTML = registrosAlmacenados.map((registro, index) => {
+    date_object.innerHTML = registrosAlmacenados.map((registro, index) => {
 
       // insertamos el html
       return `
-        <div class='card'>
-                                    <table class="table table-light">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">Nombres</th>
-                                                <th scope="col">Apellidos</th>
-                                                <th scope="col">Correo</th>
-                                                <th scope="col">Direccion Paciente</th>
-                                                <th scope="col">Tipo Documento</th>
-                                                <th scope="col">Numero Documento</th>
-                                                <th scope="col">Editar</th>
-                                                <th scope="col">Eliminar</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
                                             <tr class="">
                                                 <td scope="row">${registro.nombre}</td>
                                                 <td>${registro.apellidos}</td>
@@ -123,16 +110,10 @@ function ImprimirListado() {
                                                 <td><button type="button" class="btn btn-outline-secondary" onClick="editar(${index})">Editar</button></td>
                                                 <td> <button type="button" class="btn btn-outline-secondary" onClick="editar(${index})">Eliminar</button></td>
                                             </tr>
-                                        </tbody>
-                                    </table
-        </div>
-        </div>
         `
-    }).join('<hr />')
+    }).join('')
   }
 }
-
-
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/previousElementSibling
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/lastElementChild
